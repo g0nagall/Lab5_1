23de341a41529d84457b27cca26cabaeb1a07cbb
g0nagall <50554188+g0nagall@users.noreply.github.com>
2019-05-20 03:53:03 +0300

    Add files via upload

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab5_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			A A = new A();
+			Console.Write("Result = ");
+			Console.WriteLine(A.C(1, 2));
+		}
+	}
+
+	class A
+	{
+		private int a;
+		private int b;
+
+		public int C(int n, int m)
+		{
+			this.a = n;
+			this.b = m;
+			return n + m;
+		}
+	}
+}
